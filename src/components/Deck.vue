0375ae9c50acd4ba3b5969ff2f57d46c4f5d6903
Tom Nuorivaara <nuorivaara@gmail.com>
2018-04-12 21:13:09 +0300

    Added Deck styling, delete button, and new deck validation

--- src/components/Deck.vue
+++ src/components/Deck.vue
@@ -1,14 +1,44 @@
 <template>
-  <li>{{ id }}: {{ deckName }} - {{ code }}</li>
+  <div class="deck">
+    <button class="deck__delete-button" @click="deleteDeck()">Delete</button>
+    <p class="deck__id">{{ id }}</p>
+    <h2 class="deck__name">
+    {{ deckName }}
+    </h2>
+    <input type="text" readonly class="deck__code" :value="code" />
+    <button class="deck__copy-button" @click="copyDeck()">Copy</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Deck",
-  props: ["id", "deckName", "code"]
+  props: ["id", "deckName", "code", "deleteDeck"],
+  methods: {
+    copyDeck() {
+      // eslint-disable-next-line
+      alert("copied!");
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-// eslint-disable-next-line
+.deck {
+  position: relative;
+  box-sizing: border-box;
+  flex-grow: 1;
+  width: 45%;
+  border: 1px solid #e3e3e3;
+  padding: 1.5rem;
+  margin: 1rem;
+  &__delete-button {
+    position: absolute;
+    top: 1.5rem;
+    right: 1.5rem;
+  }
+  &__id {
+    margin-top: 0;
+  }
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="main">
-    <h1 class="main__heading">{{ title }}</h1>
-    <p class="main__intro">{{ intro }}</p>
-    <input type="text" :placeholder="placeholder.name" v-model="input.name">
-    <input type="text" :placeholder="placeholder.code" v-model="input.code">
-    <button @click="addDeck">+</button>
-    <ul class="main__decklist">
-      <deck v-for="deck in decks" :key="deck.id" :id="deck.id" :deckName="deck.name" :code="deck.code"/>
-    </ul>
+    <div class="intro">
+      <img src="../assets/logo.png">
+      <h1 class="intro__heading">{{ title }}</h1>
+      <p class="intro__tagline">{{ intro }}</p>
+    </div>
+    <div class="add-deck">
+      <input type="text" :placeholder="placeholder.name" v-model="input.name">
+      <input type="text" :placeholder="placeholder.code" v-model="input.code">
+      <button @click="addDeck">+ Add</button>
+    </div>
+    <div class="decklist">
+      <deck v-for="deck in decks" :key="deck.id" :id="deck.id" :deck-name="deck.name" :code="deck.code" :delete-deck="deleteDeck" />
+    </div>
   </div>
 </template>
 
@@ -47,7 +52,10 @@ export default {
           name: "Demon Warlock",
           code: "third code"
         }
-      ]
+      ],
+      error: {
+        emptyDeck: "Please give your deck a name and deck code!"
+      }
     };
   },
   methods: {
@@ -58,19 +66,41 @@ export default {
         name: this.input.name,
         code: this.input.code
       };
-      this.decks = [...this.decks, newDeck];
-      this.input.name = "";
-      this.input.code = "";
+      if (this.validateDeck(newDeck)) {
+        this.decks = [...this.decks, newDeck];
+        this.input.name = "";
+        this.input.code = "";
+      }
+    },
+    validateDeck(deck) {
+      if (!deck.name || deck.code) {
+        // eslint-disable-next-line
+        alert(this.error.emptyDeck);
+        return false;
+      }
+      return true;
+    },
+    deleteDeck() {
+      // eslint-disable-next-line
+      alert("Deleted!");
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.main {
-  &__decklist {
-    margin-left: 2rem;
-    text-align: left;
-  }
+.intro {
+  text-align: center;
+}
+.add-deck {
+  text-align: center;
+  margin-bottom: 2rem;
+}
+.decklist {
+  display: flex;
+  align-items: stretch;
+  flex-wrap: wrap;
+  margin: 0 2rem;
+  text-align: left;
 }
 </style>
